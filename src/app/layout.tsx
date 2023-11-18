db78997e882f74f2ba0d61c8344539ff55999f2c
BRIAN <owner@MBP-de-Brian.home>
2023-11-18 21:50:32 +0000

    Criando abas e animacoes com Radix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="pt-br">
+    <html lang="pt-br" className="antialiased">
       <body className={inter.className}>
         <div className="grid min-h-screen grid-cols-app">
           <Sidebar/>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
+import { SettingTabs } from "@/components/SettingTabs";
+
 export default function Home() {
   return (
-    <h1>Hello</h1>
+    <>
+    <h1 className="text-3xl font-medium text-zinc-900">Settings</h1>
+
+    <SettingTabs/>
+    </>
   )
 }
