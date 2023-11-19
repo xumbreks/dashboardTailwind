1cf437ba7537a6cd22115dd15dbc5c181bac55bf
BRIAN <owner@MBP-de-Brian.home>
2023-11-19 15:34:50 +0000

    estrutura do formulario

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,40 @@ import { SettingTabs } from "@/components/SettingTabs";
 export default function Home() {
   return (
     <>
-    <h1 className="text-3xl font-medium text-zinc-900">Settings</h1>
+      <h1 className="text-3xl font-medium text-zinc-900">Settings</h1>
 
-    <SettingTabs/>
+      <SettingTabs />
+
+      <div className="flex mt-6 flex-col">
+        <div className="flex items-center justify-between pb-5 border-b border-zinc-200">
+          <div className="space-y-1">
+            <h2 className="text-lg font-medium text-zinc-900">Personal info</h2>
+            <span className="text-sm text-zinc-500">
+              Update your photo and personal details here.
+            </span>
+          </div>
+          <div className="flex items-center gap-2">
+            <button
+              type="button"
+              className="rounded-lg px-4 py-2 text-sm font-semibold shadow-sm border border-zinc-300 text-zinc-700 
+              hover:bg-zinc-50"
+            >
+              Cancel
+            </button>
+            <button
+              type="button"
+              form="settings"
+              className="rounded-lg px-4 py-2 text-sm font-semibold shadow-sm bg-violet-600 text-white hover:bg-violet-700"
+            >
+              Save
+            </button>
+          </div>
+        </div>
+
+        <form id="settings" className="w-full flex flex-col mt-6 ">
+          form
+        </form>
+      </div>
     </>
-  )
+  );
 }
