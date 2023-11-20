efbd62ea111793347451b04de7f727e95e5bf72a
BRIAN <owner@MBP-de-Brian.home>
2023-11-20 14:08:12 +0000

    Listagem de Arquivos

--- src/components/Form/FileInput/index.tsx
+++ src/components/Form/FileInput/index.tsx
@@ -2,5 +2,7 @@ import { Control } from './Control'
 import { ImagePreview } from './ImagePreview'
 import { Root } from './Root'
 import { Trigger } from './Trigger'
+import { FileList } from './FileList'
 
-export { Control, ImagePreview, Root, Trigger }
+
+export { Control, ImagePreview, Root, Trigger, FileList }
