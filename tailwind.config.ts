031c50c666d1e5834541c5a4ff54c596b967dcf8
BRIAN <owner@MBP-de-Brian.home>
2023-11-20 18:50:17 +0000

    Animacoes e Acessibilidade Foco

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,8 +19,23 @@ const config: Config = {
       colors: {
         violet: {
           25 : '#fcfaff'
-        }
-      }
+        },
+      },
+      maxWidth: {
+        app: '700px',
+      },
+
+      keyframes: {
+        slideDownAndFade: {
+          from: { opacity: '0', transform: 'translateY(-2px)' },
+          to: { opacity: '1', transform: 'translateY(0)' },
+        },
+      },
+
+      animation: {
+        slideDownAndFade:
+          'slideDownAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)',
+      },
     },
   },
   plugins: [],
